acedbd108b3ebd173c4ae49c6cf5e191965cfb71
John Dooley <john.dooley1@merkleinc.com.au>
2023-10-20 16:00:30 +1100

    Session 1 example

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,23 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+          Text('rowffff'),
+          Text('row2'),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'col1',
+              ),
+              Text('col2'),
+              SizedBox(
+                  width: 100,
+                  height: 200,
+                  child: Center(child: ColoredBox(color: Colors.red))),
+            ],
+          )
+        ]),
       ),
     );
   }
